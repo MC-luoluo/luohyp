5e8ada6e1ee442af1c5acc33998fc35460770338
MC洛洛 <1592859065@qq.com>
2024-10-14 21:15:55 +0800

    Optimize code

--- src/main/java/moe/luoluo/hypixel/Counts.java
+++ src/main/java/moe/luoluo/hypixel/Counts.java
@@ -32,98 +32,38 @@ public class Counts {
         chain.append("总人数: ");
         chain.append(new PlainText(String.valueOf(json.get("playerCount").getAsInt())));
 
-        if (games.has("MAIN_LOBBY") && games.get("MAIN_LOBBY").getAsJsonObject().has("players")) {
-            chain.append("\n| 主大厅: ");
-            chain.append(new PlainText(String.valueOf(games.get("MAIN_LOBBY").getAsJsonObject().get("players").getAsInt())));
-            if (games.has("TOURNAMENT_LOBBY") && games.get("TOURNAMENT_LOBBY").getAsJsonObject().has("players")) {
-                chain.append(" | 锦标赛大厅: ");
-                chain.append(new PlainText(String.valueOf(games.get("TOURNAMENT_LOBBY").getAsJsonObject().get("players").getAsInt())));
+        String[][] gameList = {
+                {"MAIN_LOBBY","主大厅"},
+                {"IDLE","其他大厅"},
+                {"SKYBLOCK","SkyBlock"},
+                {"BEDWARS","起床战争"},
+                {"SKYWARS","空岛战争"},
+                {"DUELS","决斗游戏"},
+                {"ARCADE","街机游戏"},
+                {"BUILD_BATTLE","建筑大师"},
+                {"MURDER_MYSTERY","密室杀手"},
+                {"WOOL_GAMES","羊毛游戏"},
+                {"TNTGAMES","TNT Games"},
+                {"WALLS3","超级战墙"},
+                {"HOUSING","家园"},
+                {"PIT","天坑乱斗"},
+                {"SURVIVAL_GAMES","闪电饥饿游戏"},
+                {"LEGACY","经典游戏"},
+                {"PROTOTYPE","实验游戏"},
+                {"SUPER_SMASH","星碎英雄"},
+                {"SMP","SMP"},
+                {"UHC","UHC"},
+                {"REPLAY","replay"},
+                {"LIMBO","Limbo"}
+        };
+
+        for (String[] x : gameList) {
+            if (games.has(x[0]) && games.get(x[0]).getAsJsonObject().has("players")) {
+                chain.append(("\n| ")).append(x[1]).append(": ");
+                chain.append(new PlainText(String.valueOf(games.get(x[0]).getAsJsonObject().get("players").getAsInt())));
             }
         }
-        if (games.has("IDLE") && games.get("IDLE").getAsJsonObject().has("players")) {
-            chain.append("\n| 大厅闲置: ");
-            chain.append(new PlainText(String.valueOf(games.get("IDLE").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("SKYBLOCK") && games.get("SKYBLOCK").getAsJsonObject().has("players")) {
-            chain.append("\n| SkyBlock: ");
-            chain.append(new PlainText(String.valueOf(games.get("SKYBLOCK").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("BEDWARS") && games.get("BEDWARS").getAsJsonObject().has("players")) {
-            chain.append("\n| 起床战争: ");
-            chain.append(new PlainText(String.valueOf(games.get("BEDWARS").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("SKYWARS") && games.get("SKYWARS").getAsJsonObject().has("players")) {
-            chain.append("\n| 空岛战争: ");
-            chain.append(new PlainText(String.valueOf(games.get("SKYWARS").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("DUELS") && games.get("DUELS").getAsJsonObject().has("players")) {
-            chain.append("\n| 决斗游戏: ");
-            chain.append(new PlainText(String.valueOf(games.get("DUELS").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("ARCADE") && games.get("ARCADE").getAsJsonObject().has("players")) {
-            chain.append("\n| 街机游戏: ");
-            chain.append(new PlainText(String.valueOf(games.get("ARCADE").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("BUILD_BATTLE") && games.get("BUILD_BATTLE").getAsJsonObject().has("players")) {
-            chain.append("\n| 建筑大师: ");
-            chain.append(new PlainText(String.valueOf(games.get("BUILD_BATTLE").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("MURDER_MYSTERY") && games.get("MURDER_MYSTERY").getAsJsonObject().has("players")) {
-            chain.append("\n| 密室杀手: ");
-            chain.append(new PlainText(String.valueOf(games.get("MURDER_MYSTERY").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("WOOL_GAMES") && games.get("WOOL_GAMES").getAsJsonObject().has("players")) {
-            chain.append("\n| 羊毛游戏: ");
-            chain.append(new PlainText(String.valueOf(games.get("WOOL_GAMES").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("TNTGAMES") && games.get("TNTGAMES").getAsJsonObject().has("players")) {
-            chain.append("\n| TNT Games: ");
-            chain.append(new PlainText(String.valueOf(games.get("TNTGAMES").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("WALLS3") && games.get("WALLS3").getAsJsonObject().has("players")) {
-            chain.append("\n| 超级战墙: ");
-            chain.append(new PlainText(String.valueOf(games.get("WALLS3").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("HOUSING") && games.get("HOUSING").getAsJsonObject().has("players")) {
-            chain.append("\n| 家园: ");
-            chain.append(new PlainText(String.valueOf(games.get("HOUSING").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("PIT") && games.get("PIT").getAsJsonObject().has("players")) {
-            chain.append("\n| 天坑乱斗: ");
-            chain.append(new PlainText(String.valueOf(games.get("PIT").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("SURVIVAL_GAMES") && games.get("SURVIVAL_GAMES").getAsJsonObject().has("players")) {
-            chain.append("\n| 闪电饥饿游戏: ");
-            chain.append(new PlainText(String.valueOf(games.get("SURVIVAL_GAMES").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("LEGACY") && games.get("LEGACY").getAsJsonObject().has("players")) {
-            chain.append("\n| 经典游戏: ");
-            chain.append(new PlainText(String.valueOf(games.get("LEGACY").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("PROTOTYPE") && games.get("PROTOTYPE").getAsJsonObject().has("players")) {
-            chain.append("\n| 实验游戏: ");
-            chain.append(new PlainText(String.valueOf(games.get("PROTOTYPE").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("SUPER_SMASH") && games.get("SUPER_SMASH").getAsJsonObject().has("players")) {
-            chain.append("\n| 星碎英雄: ");
-            chain.append(new PlainText(String.valueOf(games.get("SUPER_SMASH").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("SMP") && games.get("SMP").getAsJsonObject().has("players")) {
-            chain.append("\n| SMP: ");
-            chain.append(new PlainText(String.valueOf(games.get("SMP").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("UHC") && games.get("UHC").getAsJsonObject().has("players")) {
-            chain.append("\n| UHC: ");
-            chain.append(new PlainText(String.valueOf(games.get("UHC").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("REPLAY") && games.get("REPLAY").getAsJsonObject().has("players")) {
-            chain.append("\n| replay: ");
-            chain.append(new PlainText(String.valueOf(games.get("REPLAY").getAsJsonObject().get("players").getAsInt())));
-        }
-        if (games.has("LIMBO") && games.get("LIMBO").getAsJsonObject().has("players")) {
-            chain.append("\n| Limbo: ");
-            chain.append(new PlainText(String.valueOf(games.get("LIMBO").getAsJsonObject().get("players").getAsInt())));
-        }
+
         return chain.build();
     }
 }
