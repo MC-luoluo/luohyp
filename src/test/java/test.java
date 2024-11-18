68295131e970987081096b72b673591c8e5eecd0
owomilkytea <1586843434@qq.com>
2024-11-18 14:41:23 +0800

    changed

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,17 @@
+import moe.luoluo.util.Format;
+
+import static java.lang.String.format;
+
 public class test {
     public static void main(String[] args) {
         System.out.println("human.version");
+        String test = "hello";
+        test = "world";
+        System.out.println(test);
+        String num = "0%";
+        num = format("%.2f", 2.53 / 5) + "%";
+        System.out.println(num);
+        String havent = null;
+        System.out.println(havent);
     }
 }
